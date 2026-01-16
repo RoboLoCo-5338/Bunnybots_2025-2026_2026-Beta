61524eb38d6da7a420758c1a0c002bc3d54dc50d
codeall1234 <1017061@lcps.org>
2026-01-16 16:07:24 -0500

    New ported brach created from master with all of the changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,19 +1,17 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Bunnybots_2025-2026-Imported";
+  public static final String MAVEN_NAME = "Bunnybots_2025-2026_2026-Beta-Imported";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-01-04 15:06:00 EST";
-  public static final long BUILD_UNIX_TIME = 1767557160841L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "c19abb829c03112cf5c7ae4c9fbee5a6f281ce27";
+  public static final String GIT_DATE = "2026-01-13 14:09:54 EST";
+  public static final String GIT_BRANCH = "refactor/wpilib-2026-release";
+  public static final String BUILD_DATE = "2026-01-16 16:04:34 EST";
+  public static final long BUILD_UNIX_TIME = 1768597474487L;
+  public static final int DIRTY = 1;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
