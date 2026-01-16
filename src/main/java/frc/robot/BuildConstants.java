366316d0096815715527590e210df5d6e29c6b13
codeall1234 <1017061@lcps.org>
2026-01-16 08:23:52 -0500

    Excluded .vscode/launch.json from spotless and build is successful

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Bunnybots_2025-2026_2026-Beta-Imported";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "f187a10b3e83f201d71055ae145ef21a3ed108d4";
-  public static final String GIT_DATE = "2026-01-15 17:59:22 EST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "2679a0308c3e214fa19cd45110d17385557145c6";
+  public static final String GIT_DATE = "2026-01-15 18:57:30 EST";
   public static final String GIT_BRANCH = "refactor/update-wpilib-2026-release";
-  public static final String BUILD_DATE = "2026-01-15 18:56:45 EST";
-  public static final long BUILD_UNIX_TIME = 1768521405100L;
+  public static final String BUILD_DATE = "2026-01-16 08:23:00 EST";
+  public static final long BUILD_UNIX_TIME = 1768569780127L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
