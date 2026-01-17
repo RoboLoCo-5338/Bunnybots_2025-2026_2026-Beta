abff36150d322ed277b0157ba6cdd9c36223101f
RoboLoCo, Team 5338 <921563@lcps.org>
2026-01-17 12:43:05 -0500

    Update RobotContainer.java
    not using TestCommands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,19 +13,34 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Pounds;
 import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.units.Units.Volts;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.hal.HALUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -67,6 +82,10 @@ import frc.robot.subsystems.vision.VisionConstants;
 import frc.robot.subsystems.vision.VisionIO;
 import frc.robot.subsystems.vision.VisionIOPhotonVision;
 import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
+import frc.robot.util.ProjectileSpeedUtils;
+import frc.robot.util.ProjectileTrajectoryUtils;
+import frc.robot.util.ProjectileTrajectoryUtils.FixedTrajectorySolution;
+
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
 import org.littletonrobotics.junction.Logger;
@@ -339,6 +358,23 @@ public class RobotContainer {
     return kDisplacementEntry.getDouble(1.0);
   }
 
+  private static final Translation3d hubLocation = new Translation3d(0, 0, 2);
+  private static final Translation3d shooterOffset =
+      new Translation3d(0, -Inches.of(0.1956095).in(Meters), Inches.of(16.081505).in(Meters));
+  private static final Angle shooterAltitude = Degrees.of(50);
+
+  private static AngularVelocity lastOmega = RadiansPerSecond.of(0);
+  private static int numLagFrames = 3;
+
+  private static double deadzone(double input, double zone) {
+    if (Math.abs(input) < zone) {
+      return 0.0;
+    } else {
+      return input;
+    }
+  }
+
+
   public void manualButtonBindings() {
     // drivetrain controls
     drive.setDefaultCommand(
@@ -353,7 +389,61 @@ public class RobotContainer {
             () -> (0.5) * -driverController.getRightX()));
     driverController.y().onTrue(drive.resetGyro());
 
-    driverController.a().whileTrue(TestCommands.testAutoAlign(drive, shooter, getKshooter(), getDisplacement()));
+    driverController.a().whileTrue(
+        Commands.run(
+        () -> {
+          double start = HALUtil.getFPGATime();
+
+          // TODO: essentialy euler's method, tracking position, velocity, angle, angular velocity,
+          // capping actual acceleration & angular acceleration by computed max values
+          Translation2d fieldPos = drive.getPose().getTranslation();
+          Translation3d targetDisplacement =
+              shooterOffset.plus(new Translation3d(fieldPos.getX(), fieldPos.getY(), 0));
+          // lunar converter 152cm bottom - 203cm top
+          targetDisplacement = new Translation3d(getDisplacement(), 0, (2.03+1.52)/2);
+
+          FixedTrajectorySolution solution =
+              ProjectileTrajectoryUtils.calcFiringSolution(
+                  MetersPerSecond.of(0),
+                  MetersPerSecond.of(0),
+                  targetDisplacement,
+                  shooterAltitude);
+
+          double shooterSpeedTransfer =
+              ProjectileSpeedUtils.calcSpeedTransferPercentage(
+                  ShooterConstants.ShooterSimConstants.SHOOTER_MOI,
+                  Pounds.of(0.2),
+                  Inches.of(3.0 / 2));
+          shooterSpeedTransfer *= getKshooter();
+          AngularVelocity shooterAngularVelocity =
+              ProjectileSpeedUtils.calcNecessaryWheelSpeed(
+                  solution.shooterVelocity, shooterSpeedTransfer, Inches.of(3.0 / 2));
+
+          Logger.recordOutput(
+              "DriveTest/ShooterRotationsPerSecond", shooterAngularVelocity.in(RotationsPerSecond));
+          shooter.setShooterVelocity(shooterAngularVelocity);
+
+          Angle azimuthDiff =
+              Radians.of(
+                  deadzone(
+                      (solution.azimuth.in(Radians) - drive.getPose().getRotation().getRadians()),
+                      0.05));
+
+          AngularVelocity omega = azimuthDiff.div(Seconds.of(1.0));
+          if (Math.abs(omega.in(RadiansPerSecond)) > drive.getMaxAngularSpeedRadPerSec()) {
+            omega =
+                RadiansPerSecond.of(
+                    Math.copySign(drive.getMaxAngularSpeedRadPerSec(), omega.in(RadiansPerSecond)));
+          }
+          drive.runVelocity(new ChassisSpeeds(MetersPerSecond.of(0), MetersPerSecond.of(0), omega));
+          Logger.recordOutput("DriveTest/AngularSpeed", omega.in(RadiansPerSecond));
+          lastOmega = omega;
+
+          Logger.recordOutput("DriveTest/azimuthDiff", azimuthDiff.in(Degrees));
+        },
+        drive,
+        shooter)
+    );
 
     // driver indexer controls
     driverController
