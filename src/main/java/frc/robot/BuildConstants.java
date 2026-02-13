e5a33f8885578dad6ca5baed92b8d11e21fa86a2
Ikshaan Kaul <129341670+codeall1234@users.noreply.github.com>
2026-02-13 17:41:38 -0500

    Copied build.gradle form master and included .vscode into the spotless

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,19 +1,17 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Bunnybots_2025-2026-Imported";
+  public static final String MAVEN_NAME = "Bunnybots_2025-2026_2026-Beta";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-01-04 15:06:00 EST";
-  public static final long BUILD_UNIX_TIME = 1767557160841L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "9083fa835e6efba0fbe63e16ea6db985b32e5bb1";
+  public static final String GIT_DATE = "2026-01-16 16:20:06 EST";
+  public static final String GIT_BRANCH = "refactor/moved-workflows";
+  public static final String BUILD_DATE = "2026-02-13 17:41:04 EST";
+  public static final long BUILD_UNIX_TIME = 1771022464523L;
+  public static final int DIRTY = 1;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
